750fccd286883ef06e90a8d3319b49714f009910
brilliantyy <yujia6366@163.com>
2016-03-02 23:04:42 +0800

    Update code

--- resume/js/app.js
+++ resume/js/app.js
@@ -2,6 +2,22 @@ $(function() {
 
     initPage();
 
+    var navToTop = $('nav').offset().top;
+    var aboutPageToTop = $('.about-page').offset().top;
+    var pageHeight = $('.home-page').height();
+    var dis = pageHeight * 0.073;
+
+    $(window).scroll(function() {
+        var scrollHeight = $(this).scrollTop();
+        if (scrollHeight >= navToTop) {
+            $('nav').css({'position': 'fixed', 'top' : '0px'});
+        }
+
+        if (aboutPageToTop - $(this).scrollTop() >= dis) {
+            $('nav').css({'position': 'absolute', 'top': pageHeight * 0.927 +'px'});
+        }
+    });
+
     $('nav ul li').mouseover(function() {
         $(this).children().css('color', '#ffffff');
     }).mouseout(function() {
@@ -33,5 +49,9 @@ $(function() {
         $('.works-page').css({'top': 3 * height + 'px'});
         $('.education-page').css({'top': 4 * height + 'px'});
         $('.contact-page').css({'top': 6 * height + 'px'});
+
+        $('nav').css({'height': height * 0.073 + 'px', 'top': height * 0.927 + 'px'});
     }
+
+
 });

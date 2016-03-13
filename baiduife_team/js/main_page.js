fae0d7b29143fc7d234cf8990810c6f5e8afb358
brilliantyy <yujia6366@163.com>
2016-03-13 16:18:13 +0800

    team page

---
+++ baiduife_team/js/main_page.js
@@ -0,0 +1,9 @@
+(function($) {
+
+    $('div.tag-container span').hover(function() {
+        $(this).css('letterSpacing', '9px');
+    }, function() {
+        $(this).css('letterSpacing', '5px');
+    })
+}(jQuery));
+
